f845e448dabb3b4e79a1c85fc514e20790c079dc
Anderfoxx <anderfox1988@gmail.com>
2024-05-21 22:04:21 -0300

    Initial commit

---
+++ app/src/main/java/com/slackmobile/cursoandroidstudiocomfirebase/activity/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.slackmobile.cursoandroidstudiocomfirebase.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.slackmobile.cursoandroidstudiocomfirebase.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LinearLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+
+
+        });
+
+        }
+    public void cadastrar(View v){
+        Intent i = new Intent (this,CadastroActivity.class) ;
+        startActivity(i);
+
+
+
+    }
+}
